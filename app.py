776c5d5dc9038cb38df43f7720f4d3fdd47203e5
KenaiGit <valarmorghulisvaleria@gmail.com>
2025-05-28 13:29:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,6 @@ async def root():
 
 if __name__ == "__main__":
     
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
 
